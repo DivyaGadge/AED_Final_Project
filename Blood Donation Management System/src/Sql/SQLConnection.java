ec535237742e76efc3be47527a6b044eed507b9c
DivyaGadge <gadge.d@northeastern.edu>
2022-12-10 16:20:11 -0500

    DB connection created

--- Blood Donation Management System/src/Sql/SQLConnection.java
+++ Blood Donation Management System/src/Sql/SQLConnection.java
@@ -8,14 +8,14 @@ import java.sql.*;
 
 /**
  *
- * @author surajvisvesh
+ * @author divyagagde
  */
 public class SQLConnection {
     
 //    Connection con = null;
     static String url = "jdbc:mysql://localhost:3306/bdms";
     private static String user = "root";
-    private static String password = "admin@123";
+    private static String password = "Linkedin@123";
     public static Connection establishConnection() {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
